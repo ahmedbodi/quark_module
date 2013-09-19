08d7c93a8822c0a623c0f4c47860eb5376bfd894
ahmedbodi <ahmed_bodi@msn.com>
2013-09-19 11:41:00 +0100

    Create Quark Setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 from distutils.core import setup, Extension
 
-ltc_scrypt_module = Extension('ltc_scrypt',
-                               sources = ['scryptmodule.c',
-                                          'scrypt.c'],
-                               include_dirs=['.'])
+quark_hash_module = Extension('quark_hash',
+                               sources = ['quarkmodule.c',
+                                          'quark.c',
+										  'sha3/blake.c',
+										  'sha3/bmw.c',
+										  'sha3/groestl.c',
+										  'sha3/jh.c',
+										  'sha3/keccak.c',
+										  'sha3/skein.c'],
+                               include_dirs=['.', './sha3'])
 
-setup (name = 'ltc_scrypt',
+setup (name = 'quark_hashs',
        version = '1.0',
-       description = 'Bindings for scrypt proof of work used by Litecoin',
-       ext_modules = [ltc_scrypt_module])
+       description = 'Bindings for quark proof of work used by Quarkcoin',
+       ext_modules = [quark_hash_module])
